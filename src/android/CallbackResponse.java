057c86baef7d818014f26ff19a8c716f9e211b1f
PauloGrijp <pxavierf@gmail.com>
2023-02-25 15:50:15 -0300

    Update CallbackResponse.java

--- src/android/CallbackResponse.java
+++ src/android/CallbackResponse.java
@@ -21,7 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
-package co.frontyard.cordova.plugin.exoplayer;
+package co.PauloGrijp.cordova.plugin.exoplayer;
 
 import org.apache.cordova.*;
 import org.json.*;
